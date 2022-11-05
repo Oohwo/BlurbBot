93cef003f137a506bf2f3efe75816795e21a859c
Oohwo <Oohwo4h@gmail.com>
2022-11-05 04:16:21 -0400

    switched order of classes ya

--- cogs/TestingButtons.py
+++ cogs/TestingButtons.py
@@ -4,16 +4,6 @@ from discord import app_commands
 
 click_counter = 0
 
-class button_view(discord.ui.View):
-  def __init__(self):
-    super().__init__(timeout=None)
-  
-  @discord.ui.button(label = "Click Me!!!!", style = discord.ButtonStyle.green, custom_id = "what even is this")
-  async def click_me(self, interaction: discord.Interaction, button: discord.ui.Button) -> None:
-    global click_counter
-    click_counter += 1
-    await interaction.response.edit_message(content = f"You clicked me {click_counter} times! :O")
-
 class TestingButtons(commands.Cog):
   def __init__(self, bot: commands.Bot):
     self.bot = bot
@@ -26,3 +16,13 @@ class TestingButtons(commands.Cog):
 async def setup(bot: commands.Bot):
   '''adds cog to bot'''
   await bot.add_cog(TestingButtons(bot))
+
+class button_view(discord.ui.View):
+  def __init__(self):
+    super().__init__(timeout=None)
+  
+  @discord.ui.button(label = "Click Me!!!!", style = discord.ButtonStyle.green, custom_id = "what even is this")
+  async def click_me(self, interaction: discord.Interaction, button: discord.ui.Button) -> None:
+    global click_counter
+    click_counter += 1
+    await interaction.response.edit_message(content = f"You clicked me {click_counter} times! :O")
